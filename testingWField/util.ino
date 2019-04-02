f36da248da7436bbc9a67a035d9e5047850b993d
Nathan <nathansd1098@gmail.com>
2019-04-02 15:10:36 -0400

    moved some functions back

--- testingWField/util.ino
+++ testingWField/util.ino
@@ -1,8 +1,3 @@
-#include <Enes100.h>
-#include <math.h>
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
-
 // Gives the angle relative to the horizontal from OSV to target.
 float angleTo(float x, float y) {
     float delX = x-getX();
@@ -31,43 +26,4 @@ float getY(){
 // Returns OSV Theta
 float getTheta(){
   return locT;
-}
-
-// Returns ultrasonic distance
-float getUltraDistance(int trig, int echo){
-  digitalWrite(trig, LOW); 
-  delayMicroseconds(2); 
-  digitalWrite(trig, HIGH); 
-  delayMicroseconds(10); 
-  digitalWrite(trig, LOW);
-  return ((pulseIn(echo, HIGH)*.0343)/2.0)*10.0/11.0;
-}
-
-// Returns true if there is an obstacle detected.
-bool obstacle(){
-  return getUltraDistance(TRIG_PIN, ECHO_PIN)<OBSTACLE_TRIGGER_DISTANCE;
-}
-
-// Sets drive motors speeds
-// -255<=speed<=255
-void setSpeed(int left, int right){
-  if(left<0){
-    frontLeftMotor->run(BACKWARD);
-    backLeftMotor->run(BACKWARD);
-  } else {
-    frontLeftMotor->run(FORWARD);
-    backLeftMotor->run(FORWARD);
-  }
-  if(right<0){
-    frontRightMotor->run(BACKWARD);
-    backRightMotor->run(BACKWARD);
-  } else {
-    frontRightMotor->run(FORWARD);
-    backRightMotor->run(FORWARD);
-  }
-  
-  backRightMotor->setSpeed(abs(right));
-  frontRightMotor->setSpeed(abs(right));
-  frontLeftMotor->setSpeed(abs(left));
-  backLeftMotor->setSpeed(abs(left));
 }
