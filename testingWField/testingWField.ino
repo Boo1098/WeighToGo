78ed4baa7cbc06037f0678fb443809482d1ee922
Boo1098 <nathansd1098@gmail.com>
2019-04-02 13:44:02 -0400

    documentation

--- testingWField/testingWField.ino
+++ testingWField/testingWField.ino
@@ -4,14 +4,19 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
+// Trigger pin of ultrasonic sensor
 #define TRIG_PIN 8 
+// Echo pin of ultrasonic sensor
 #define ECHO_PIN 7
+
+// I'm lazy
 #define desX Enes100.destination.x
 #define desY Enes100.destination.y
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();  
 
+// Create the 4 motors on the OSV
 Adafruit_DCMotor *backRightMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *frontRightMotor = AFMS.getMotor(2);
 Adafruit_DCMotor *frontLeftMotor = AFMS.getMotor(3);
@@ -20,13 +25,15 @@ Adafruit_DCMotor *backLeftMotor = AFMS.getMotor(4);
 void setup() {
   // Team Name, Mission Type, Marker ID, RX Pin, TX Pin
   Enes100.begin("Weight to go", DEBRIS, 3, 8, 9);
-  
+
+  // Print out destination location
   Enes100.print("Destination is at (");
   Enes100.print(desX);
   Enes100.print(", ");
   Enes100.print(desY);
   Enes100.println(")");
 
+  // Set pin modes of ultrasonic sensor
   pinMode(TRIG_PIN, OUTPUT); 
   pinMode(ECHO_PIN, INPUT);
 
@@ -38,33 +45,40 @@ void setup() {
   }
 }
 
+// Anything that needs to be ran every tick goes here
 void updateEverything(){
   Enes100.updateLocation();
 }
 
+// Returns distance from OSV to (x,y) location
 float getDistance(float x, float y){
   return sqrt(sq(x-getX())+sq(y-getY()));
 }
 
+// Returns angle between Horizontal, OSV location, and target (x,y)
 float getAngle(float x, float y){
   float delX = x-getX();
   float delY = y-getY();
   return atan2(delY,delX);
 }
 
+// Returns OSV X
 float getX(){
   return Enes100.location.x;
 }
 
+// Returns OSV Y
 float getY(){
   return Enes100.location.y;
 }
 
+// Returns OSV Theta
 float getTheta(){
   return Enes100.location.theta;
 }
 
-float getDistance(int trig, int echo){
+// Returns ultrasonic distance
+float getUltraDistance(int trig, int echo){
   digitalWrite(trig, LOW); 
   delayMicroseconds(2); 
   digitalWrite(trig, HIGH); 
@@ -73,6 +87,8 @@ float getDistance(int trig, int echo){
   return ((pulseIn(echo, HIGH)*.0343)/2.0)*10.0/11.0;
 }
 
+// Sets drive motors speeds
+// -255<=speed<=255
 void setSpeed(int left, int right){
   if(left<0){
     frontLeftMotor->run(BACKWARD);
