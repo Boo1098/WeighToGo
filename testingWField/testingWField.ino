2ee96e52b0fb28f9f4e2e7f56a6932f6a7c0fe53
Nathan <nathansd1098@gmail.com>
2019-04-08 22:15:29 -0400

    golf

--- testingWField/testingWField.ino
+++ testingWField/testingWField.ino
@@ -247,9 +247,9 @@ void avoidObstacle() {
   // }
 
   // Why write many line when few line do trick?
-  double newY = (locY > 1.333 || locY < 0.333) ? 1 : (locY > 1 ? 1.666 : 0.333);
+  double newY = locY > 1.333 || locY < 0.333 ? 1 : locY > 1 ? 1.666 : 0.333;
   double middleY = locY > 1 ? 1.333 : 0.666;
-  orient((locY > 1.333 || (locY < 1 && locY > 0.666)) ? -1.57 : 1.57);
+  orient(locY > 1.333 || (locY < 1 && locY > 0.666) ? -1.57 : 1.57);
   driveFar(locX, middleY, false);
   driveFar(locX + 0.4, newY, false);
 
