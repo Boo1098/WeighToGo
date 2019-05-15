39062f5392a4897c9b6b6afbd594111d104a0656
Nathan <nathansd1098@gmail.com>
2019-05-15 19:14:33 -0400

    cleanup

--- testingWField/testingWField.ino
+++ testingWField/testingWField.ino
@@ -20,19 +20,12 @@ int pos = 0;
 #define OBSTACLE_TRIGGER_DISTANCE 11.5
 // Distance we want the far navigation to end up from the mission site
 #define DESTINATION_BUFFER_DISTANCE 0.25
-// Max speed of OSV. Not currently implemented
-#define MAX_SPEED 255.0
 // Minimum speed OSV requires to move forwards
 #define MIN_SPEED 30.0
 // Proportional factor for driveFar corrections
 #define DRIVE_FAR_kP 255.0 * 5.0 / 3.14
 // Proportional factor for orient corrections
 #define ORIENT_kP ((255.0 - MIN_SPEED_TURN) * 2.0 / 3.14)
-// I like FUDGE
-#define FUDGE 0
-#define PERSPECTIVE 0.07
-// Weight of claw with nothing on it.
-#define BASE_WEIGHT 0
 // Distance away to approach
 #define APPROACH_DIST 0.5
 
@@ -45,7 +38,14 @@ int pos = 0;
 #define DOUT 3
 #define CLK 2
 
-// I'm lazy
+// Servo pin
+#define SERVO_PIN 9
+
+// Limit switch pins
+#define LOWER_LIMIT_SWITCH 8
+#define UPPER_LIMIT_SWITCH 13
+
+// Makes getting location values easier
 #define desX Enes100.destination.x
 #define desY Enes100.destination.y
 #define locX Enes100.location.x
@@ -65,17 +65,108 @@ Adafruit_DCMotor *backLeftMotor = AFMS.getMotor(4);
 // Create scale object
 HX711 scale;
 
+// Create Servo Object
 Servo myservo;
+
+// Create Magneto Object
 DFRobot_QMC5883 compass;
-double baseline;
 
 // Magic
 float calibration_factor = -242;
 
 void setup() {
+  // Initialize OSV
+  bootUp();
+  Enes100.println("OSV Initialized.");
+
+  // Make sure arm is lifted all the way
+  myservo.attach(SERVO_PIN);
+  myservo.write(91);
+  liftArm();
+  myservo.attach(15);
+
+  // Moves OSV to one of the three colunms.
+  startUp();
+
+  // Drive to the target to buffer distance.
+  driveFar(Enes100.destination.x - DESTINATION_BUFFER_DISTANCE, locY, true);
+
+  // Move to be above or below target by APPROACH_DIST meters
+  double targetY = getColumn(desY) == 1 ? desY + APPROACH_DIST : desY - APPROACH_DIST;
+  orient((locY - targetY) > 0 ? -1.57 : 1.57);
+  driveFar(locX, targetY, false);
+  orient(0);
+
+  // Drive to be aligned with target on y-axis.
+  driveClose(desX - locX + 0.06);
+
+  // Point towards debris
+  orient((locY - desY) > 0 ? -1.57 : 1.57);
+
+  // Back up if too close
+  if (distanceTo(desX, desY) < .50) {
+    driveClose(distanceTo(desX, desY) - 0.5);
+  }
+
+  // ReAttach Servo
+  Enes100.println("Attaching Servo");
+  myservo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object
+  myservo.write(91);
+  Enes100.println("Servo Attached");
+
+  // Lower Arm
+  Enes100.println("Lowering Arm");
+  lowerArm();
+  Enes100.println("Arm Lowered");
+
+  // UnAttach Servo
+  myservo.write(91);
+  myservo.attach(15);
+
+  // Measure magneto baseline
+  Enes100.println("Measuring Baseline");
+  delay(1000);
+  double baseline = magneto();
+  Enes100.print("Baseline: ");
+  Enes100.println(baseline);
+
+  // Drive up close
+  driveClose(distanceTo(desX, desY) - .13);
+  updateEverything();
+
+  // Take magneto measuring now that debris is loaded
+  double measurement = magneto();
+  Enes100.println(measurement);
+
+  // Reattach Servo
+  myservo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object
+  myservo.write(91);
+
+  // Lift arm
+  Enes100.println("Lifting Arm");
+  liftArm();
+  Enes100.println("Arm Lifted");
+
+  // Drop arm just a little bit.
+  delay(1000);
+
+  // Return if debris is steel or copper
+  if (steelCheck(baseline, measurement)) {
+    Enes100.mission(STEEL);
+  } else {
+    Enes100.mission(COPPER);
+  }
+
+  // Return measured weight
+  delay(2000);
+  Enes100.mission(getWeight(20));
+}
+
+// All Initialization Code
+void bootUp() {
   // Limit Switch Setup
-  pinMode(8, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);
+  pinMode(LOWER_LIMIT_SWITCH, INPUT_PULLUP);
+  pinMode(UPPER_LIMIT_SWITCH, INPUT_PULLUP);
 
   delay(10000);
 
@@ -134,87 +225,6 @@ void setup() {
   scale.set_scale(calibration_factor);
   scale.tare();  //Reset the scale to 0
   Enes100.println("Scale Initialized");
-
-  // Make sure arm is lifted all the way
-  myservo.attach(9);
-  myservo.write(91);
-  liftArm();
-  myservo.attach(15);
-
-  // Moves OSV to one of the three colunms.
-  startUp();
-
-  // Drive to the target close enough.
-  driveFar(Enes100.destination.x - DESTINATION_BUFFER_DISTANCE, locY, true);
-
-  double targetY = getColumn(desY) == 1 ? desY + APPROACH_DIST : desY - APPROACH_DIST;
-  orient((locY - targetY) > 0 ? -1.57 : 1.57);
-  driveFar(locX, targetY, false);
-  orient(0);
-
-  // Line up with debris on y-axis
-  driveClose(desX - locX + 0.06);
-
-  // Point towards debris
-  orient((locY - desY) > 0 ? -1.57 : 1.57);
-
-  // Back up if too close
-  if (distanceTo(desX, desY) < .50) {
-    driveClose(-(.5 - distanceTo(desX, desY)));
-  }
-
-  // ReAttach Servo
-  Enes100.println("Attaching Servo");
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo.write(91);
-  Enes100.println("Servo Attached");
-
-  // Lower Arm
-  Enes100.println("Lowering Arm");
-  lowerArm();
-  Enes100.println("Arm Lowered");
-
-  // UnAttach Servo
-  myservo.write(91);
-  myservo.attach(15);
-
-  // Measure magneto baseline
-  Enes100.println("Measuring Baseline");
-  delay(1000);
-  baseline = magneto();
-  Enes100.print("Baseline: ");
-  Enes100.println(baseline);
-
-  // Drive up close
-  driveClose(distanceTo(desX, desY) - .13);
-  updateEverything();
-
-  // Take magneto measuring now that debris is loaded
-  double measurement = magneto();
-  Enes100.println(measurement);
-
-  // Reattach Servo
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo.write(91);
-
-  // Lift arm
-  Enes100.println("Lifting Arm");
-  liftArm();
-  Enes100.println("Arm Lifted");
-
-  // Drop arm just a little bit.
-  delay(1000);
-
-  // Return if debris is steel or copper
-  if (steelCheck(baseline, measurement)) {
-    Enes100.mission(STEEL);
-  } else {
-    Enes100.mission(COPPER);
-  }
-
-  // Return measured weight
-  delay(2000);
-  Enes100.mission(getWeight(20));
 }
 
 // Drives to a point on the field with obstacle avoidance if obsCheck = true.
@@ -301,7 +311,8 @@ void driveFar(double x, double y, bool obsCheck) {
   }
 }
 
-// Drives slowly a distance in m.
+// Drives slowly a distance in m
+// if dist>0 goes forwards, if dist<0 goes backwards
 void driveClose(double dist) {
   boolean negative = false;
   if (dist < 0) {
@@ -339,7 +350,8 @@ void startUp() {
   updateEverything();
 
   // Depending on column, drives to center of nearest.
-  if (!((locY - 0.333 > 0 ? locY - 0.333 : 0.333 - locY) < 0.2 || (locY - 1 > 0 ? locY - 1 : 1 - locY) < 0.2 || (locY - 1.666 > 0 ? locY - 1.666 : 1.666 - locY) < 0.2)) {
+  // Tolerance of .2 meters from center of column to activate
+  if (!(fabs(locY - 0.333) < 0.2 || fabs(locY - 1) < 0.2 || fabs(locY - 1.666) < 0.2)) {
     if (locY < 0.66) {
       orient(locY < 0.33 ? 1.57 : -1.57);
       driveFar(locX, 0.33, false);
@@ -351,6 +363,7 @@ void startUp() {
       driveFar(locX, 1.66, false);
     }
   }
+
   // Orients self to get ready.
   orient(0);
 
@@ -464,6 +477,7 @@ void setMotorSpeed(int left, int right) {
 }
 
 // Returns weight measured by load cell.
+// int samples - how many samples are averaged together for the reading
 double getWeight(int samples) {
   scale.set_scale(calibration_factor);
   return scale.get_units(samples);
@@ -485,19 +499,18 @@ void printStats() {
   }
 }
 
+// Returns reading from magnetometers Y axis
 double magneto() {
   Vector mag = compass.readRaw();
   delay(500);
   return mag.YAxis;
 }
 
+// Given a baseline reading and test reading, returns true if material is steel, false if otherwise
 boolean steelCheck(double baseline, double test) {
+  Enes100.print("Delta: ");
   Enes100.println(baseline - test);
-  if (fabs(baseline - test) > 1500) {
-    return true;
-  } else {
-    return false;
-  }
+  return fabs(baseline - test) > 1500;
 }
 
 void lowerArm() {
@@ -507,9 +520,9 @@ void lowerArm() {
   }
 
   //While loop checks it button is activated
-  int sensorVal = digitalRead(8);  //Read digital port 8
+  int sensorVal = digitalRead(LOWER_LIMIT_SWITCH);
   while (sensorVal == HIGH) {
-    sensorVal = digitalRead(8);
+    sensorVal = digitalRead(LOWER_LIMIT_SWITCH);
   }
 
   for (pos = 95; pos >= 91; pos -= 1) {
@@ -520,22 +533,48 @@ void lowerArm() {
 }
 
 void liftArm() {
-  for (pos = 91; pos >= 81; pos -= 1)  //Accelerates to full CW
-  {
+  //Accelerates to full CW
+  for (pos = 91; pos >= 81; pos -= 1) {
     myservo.write(pos);  // tell servo to go to position in variable 'pos'
     delay(50);           // waits 15ms for the servo to reach the position
   }
 
-  int sensorVal2 = digitalRead(13);
-  while (sensorVal2 == HIGH) {  //MAYBE USE HIGH OR LOW INSTEAD OF 1
-    sensorVal2 = digitalRead(13);
+  int sensorVal2 = digitalRead(UPPER_LIMIT_SWITCH);
+  while (sensorVal2 == HIGH) {
+    sensorVal2 = digitalRead(UPPER_LIMIT_SWITCH);
     if (sensorVal2 == LOW) {
-      Serial.println("Button Triggered!");
       for (pos = 81; pos <= 87; pos += 1) {
         myservo.write(pos);  // tell servo to go to position in variable 'pos'
-        delay(10);           // waits 15ms for the servo to reach the position
+        delay(10);           // waits 10ms for the servo to reach the position
       }
       break;  //Break from loop for safe measure
     }
   }
+}
+
+// Gives the angle relative to the horizontal from OSV to target.
+double angleTo(double x, double y) {
+  return atan2(y - locY, x - locX);
+}
+
+// Returns the distance from the OSV to the coordinate passed in
+double distanceTo(double x, double y) {
+  return sqrt(sq(locX - x) + sq(locY - y));
+}
+
+// Anything that needs to be ran every tick goes here
+void updateEverything() {
+  while (!updateLocation())
+    ;
+  // Make sure location given makes sense
+  while (!((locY < 2 && locY > 0) && (locX < 4 && locX > 0) && (locT > -3.2 && locT < 3.2))) {
+    Enes100.println("Bad location");
+    updateLocation();
+  }
+  printStats();
+}
+
+// Returns the row that a y coordinate is in if the field were split into 3 rows.
+int getColumn(double y) {
+  return y < 0.666 ? 1 : (y < 1.333 ? 2 : 3);
 }
